9cf1ec8f5ea5452a511b593be351dd99151be9b2
Hunfrit <Artem.Hunfrit@gmail.com>
2017-08-22 16:04:27 +0300

    Change function

--- app/src/main/java/com/hunfrit/geolocation/MainActivity.java
+++ app/src/main/java/com/hunfrit/geolocation/MainActivity.java
@@ -1,19 +1,30 @@
 package com.hunfrit.geolocation;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    SharedPreferences sPref;
-    TextView tv;
+    SharedPreferences sPref, sharedPreferences;
+    TextView startPoint, currentLocation, distanceTV;
+    String LIT, LON;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +32,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-        tv = (TextView) findViewById(R.id.textView);
+        startPoint = (TextView) findViewById(R.id.startPoint);
+        currentLocation = (TextView) findViewById(R.id.currentLocation);
+        distanceTV = (TextView) findViewById(R.id.distance);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d("TAGA", "WTF");
+
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 10, 10, locationListener);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,28 +63,112 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
 
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                intent.putExtra("lit", getIntent().getSerializableExtra("lit"));
-                intent.putExtra("lon", getIntent().getSerializableExtra("lon"));
+                intent.putExtra("lit", LIT);
+                intent.putExtra("lon", LON);
                 startActivity(intent);
             }
         });
     }
 
-    public void SAVE (View view){
-        String first = (String) getIntent().getSerializableExtra("lit");
-        String second = (String) getIntent().getSerializableExtra("lon");
+    public void startTracking (View view){
+        String first = LIT;
+        String second = LON;
         sPref = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor ed = sPref.edit();
         ed.putString("LIT", first);
         ed.putString("LON", second);
         ed.commit();
+        startPoint.setText(first + ", " + second);
     }
 
     public void LOAD(View view){
         sPref = getPreferences(MODE_PRIVATE);
         String firstSaved = sPref.getString("LIT", "");
         String secondSaved = sPref.getString("LON", "");
-        tv.setText(firstSaved + ", " + secondSaved);
+        startPoint.setText(firstSaved + ", " + secondSaved);
     }
 
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(final Location location) {
+            Toast.makeText(getApplicationContext(), "Location changed", Toast.LENGTH_SHORT).show();
+
+            if (location.getProvider().equals(LocationManager.NETWORK_PROVIDER)){
+                LIT = String.format("%1$.4f", location.getLatitude());
+                LON = String.format("%1$.4f", location.getLongitude());
+                currentLocation.setText(LIT + ", " + LON);
+
+                if (sPref != null){
+
+                    sPref = getPreferences(MODE_PRIVATE);
+                    String firstSaved = sPref.getString("LIT", "");
+                    String secondSaved = sPref.getString("LON", "");
+                    startPoint.setText(firstSaved + ", " + secondSaved);
+                    Location locationA = new Location("point A");
+                    Location locationB = new Location("point B");
+
+                    if (sharedPreferences == null){
+                        locationA.setLatitude(Double.parseDouble(sPref.getString("LIT", "")));
+                        locationA.setLongitude(Double.parseDouble(sPref.getString("LON", "")));
+
+                        locationB.setLatitude(Double.parseDouble(LIT));
+                        locationB.setLongitude(Double.parseDouble(LON));
+                    } else if (sharedPreferences != null){
+                        locationA.setLatitude(Double.parseDouble(sharedPreferences.getString("PreviousLIT", "")));
+                        locationA.setLongitude(Double.parseDouble(sharedPreferences.getString("PreviousLON", "")));
+
+                        locationB.setLatitude(Double.parseDouble(LIT));
+                        locationB.setLongitude(Double.parseDouble(LON));
+                    }
+                    float distance = locationA.distanceTo(locationB);
+                    sPref = getPreferences(MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sPref.edit();
+                    editor.putFloat("PreviousDistance", distance);
+                    float currentDistance = distance + sPref.getFloat("PreviousDistance", 0);
+                    if (currentDistance >= 1000){
+                        currentDistance /= 1000;
+                        distanceTV.setText(String.valueOf(String.format("%1$.2f", currentDistance)) + " km");
+                    } else{
+                        distanceTV.setText(String.valueOf(String.format("%1$.1f", currentDistance)) + " m");
+                    }
+                } else if (sPref == null){
+                    distanceTV.setText("0.0 m");
+                }
+
+                sharedPreferences = getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor previousEditor = sharedPreferences.edit();
+                previousEditor.putString("PreviousLIT", LIT);
+                previousEditor.putString("PreviousLON", LON);
+                previousEditor.commit();
+
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Toast.makeText(getApplicationContext(), "Please, turn on location", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+
+        public void onProviderEnabled(String provider) {
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+            }
+        }
+    };
 }
--- app/src/main/java/com/hunfrit/geolocation/SplashScreen.java
+++ app/src/main/java/com/hunfrit/geolocation/SplashScreen.java
@@ -1,17 +1,11 @@
 package com.hunfrit.geolocation;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
+
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.util.Log;
-import android.widget.Toast;
+
 
 
 /**
@@ -20,71 +14,25 @@ import android.widget.Toast;
 
 public class SplashScreen extends Activity {
 
-    private LocationManager locationManager;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
 
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Log.d("TAGA", "WTF");
-            Intent intent = new Intent(SplashScreen.this, MapsActivity.class);
-            startActivity(intent);
-            finish();
-            return;
-        }
-
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 10, 10, locationListener);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    java.util.concurrent.TimeUnit.MILLISECONDS.sleep(3000);
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
-
-    private LocationListener locationListener = new LocationListener() {
-
-        @Override
-        public void onLocationChanged(Location location) {
-            Toast.makeText(getApplicationContext(), "Location changed", Toast.LENGTH_SHORT).show();
-            if (location.getProvider().equals(LocationManager.NETWORK_PROVIDER)){
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                intent.putExtra("lit", String.format("%1$.4f", location.getLatitude()));
-                intent.putExtra("lon", String.format("%1$.4f", location.getLongitude()));
-                startActivity(intent);
-                finish();
-            }
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            Toast.makeText(getApplicationContext(), "Please, turn on location", Toast.LENGTH_LONG).show();
-        }
-
-        @Override
-
-        public void onProviderEnabled(String provider) {
-            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
-            }
-        }
-    };
 }
